b9f2ac19bc8e68496f9889a3875d0d47a1e958ed
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-01-16 20:20:37 +0100

    Servidor & DB

---
+++ server/server.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Field = require('../models/field')
+
+var app = express();
+
+var port = process.env.PORT ||  3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/api/field', (req, res) => {
+    res.status(200).send({ field: [] });
+});
+
+app.get('/api/field/:fieldId', (req, res) => {
+
+});
+
+app.post('/api/field', (req, res) => {
+    console.log('POST /api/field');
+    console.log(req.body);
+    
+    let field = Field();
+    field.name = req.body.name;
+    field.description = req.body.description;
+    field.image = req.body.image;
+    field.city = req.body.city;
+    field.category = req.body.category;
+});
+
+app.put('/api/field/:fieldId', (req, res) => {
+
+});
+
+app.delete('/api/field/:fieldId', (req, res) => {
+
+});
+
+mongoose.connect('mongodb://localhost:27017/api', (err, res) => {
+    if(err) {
+        return console.log(`Error al conectar con la base de datos: ${err}`);
+    }
+    console.log('Conexión a la base de datos realizada');
+
+    app.listen(port, () => {
+        console.log(`Servidor iniciado en http://localhost:${port}`);
+    })
+});
