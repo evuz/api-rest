88d7f3335321c0206a875134bff34509ec4e7dda
evuz <jesus_jgb_45@hotmail.com>
2017-01-17 23:55:38 +0100

    route: auth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const FieldCtrl = require('../controllers/field')
+const auth = require('../middlewares/auth');
 const api = express.Router();
 
 api.get('/field', FieldCtrl.getFields);
@@ -9,5 +10,8 @@ api.get('/field/:fieldId', FieldCtrl.getField);
 api.post('/field', FieldCtrl.saveField);
 api.put('/field/:fieldId', FieldCtrl.updateField);
 api.delete('/field/:fieldId', FieldCtrl.deleteField);
+api.get('/private', auth.isAuth, function (req, res) {
+    res.status(200).send({ message: 'Tienes acceso' });
+});
 
 module.exports = api;
