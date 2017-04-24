6131de087f3b9402a44a49548591e401c78c55ae
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-04-24 20:37:25 +0200

    Add routes signUp & signIn

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const FieldCtrl = require('../controllers/field')
+const UserCtrl = require('../controllers/user');
 const auth = require('../middlewares/auth');
 const api = express.Router();
 
@@ -10,6 +11,8 @@ api.get('/field/:fieldId', FieldCtrl.getField);
 api.post('/field', FieldCtrl.saveField);
 api.put('/field/:fieldId', FieldCtrl.updateField);
 api.delete('/field/:fieldId', FieldCtrl.deleteField);
+api.post('/signup', UserCtrl.signUp);
+api.post('/signin', UserCtrl.signIn);
 api.get('/private', auth.isAuth, function (req, res) {
     res.status(200).send({ message: 'Tienes acceso' });
 });
