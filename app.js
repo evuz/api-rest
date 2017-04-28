b7aa56da06b992d288f31df59ab9b76de4083565
evuz <jesus_jgb_45@hotmail.com>
2017-04-28 13:00:03 +0200

    Routes: / & /login

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ app.engine('.hbs', hbs({
 app.set('view engine', '.hbs');
 
 app.use('/api', api);
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
     res.render('product')
+})
+app.get('/login', (req, res) => {
+    res.render('login')
 })
 
 module.exports = app;
