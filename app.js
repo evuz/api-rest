ea88795dd24fad9177a15691c31eeff08d85ca23
evuz <jesus_jgb_45@hotmail.com>
2017-05-18 10:56:58 +0200

    Add favicon

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 'use strict'
 
+const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
-const app = express();
 const api = require('./routes');
 const cors = require('./middlewares/cors');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors);
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')))
 app.use(express.static('public'));
 app.engine('.hbs', hbs({
     defaultLayout: 'default',
