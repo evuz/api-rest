918c4463a929ab1f488ddf5d546726a6b76cbdb8
a170828 <acalvoe@externos.bankia.com>
2017-05-26 09:47:40 +0200

    app: fix favicon route

--- server/app.ts
+++ server/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors);
-app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '..', 'public', 'img', 'favicon.ico')))
 app.use(express.static('public'));
 app.engine('.hbs', hbs({
     defaultLayout: 'default',
